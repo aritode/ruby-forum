bd009e8033728a2f9abba0616428d6e5f0da2127
Ken <KenIovino@gmail.com>
2012-08-14 22:19:06 -0400

    Added a no permissions error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
   protect_from_forgery
+  
+  # renders a no permissions error page
+  def render_no_permissions
+   render "errors/permissions"
+  end
+  
 end
