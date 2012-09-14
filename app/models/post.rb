fda016397c5899fe05752a5a76bbd7a6a4d47796
Ken <KenIovino@gmail.com>
2012-09-14 17:30:49 -0400

    Cleaned up the recent post processes in the post's callbacks

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,20 +9,20 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :content
   
-  # update the counters and last post details after post creation
+  # update the counters and last post details after a post is created
   after_create do |p|
     # first posts don't count towards the post/reply counters, so we skip it
     if p.topic.posts.length > 1
       p.topic.update_attribute(:replies, p.topic.replies + 1)
       p.topic.forum.update_attribute(:post_count, p.topic.forum.post_count + 1)
-      p.topic.forum.update_attribute(:last_post_id, p.topic.forum.recent_post.nil? ? 0 : p.topic.forum.recent_post.id)
+      rebuild_recent_post
     end
 
     p.user.update_attribute(:post_count, p.user.post_count + 1)
     p.topic.update_attribute(:last_post_at, Time.now)
   end
 
-  # update counters when certain actions have been preformed, E.g. when a post is "soft-deleted"
+  # update counters when certain actions have been preformed, E.g. when a post is "soft-deleted", etc.
   after_update do |p|
     # if soft-deleting or unappoving
     if (p.visible_was == 1 and p.visible == 2) or (p.visible_was == 1 and p.visible == 0)
@@ -39,23 +39,20 @@ class Post < ActiveRecord::Base
       User.increment_counter :post_count, p.user_id
       User.decrement_counter :post_count, p.user_id_was
     end
+    
+    rebuild_recent_post
   end
   
   # update stats and last post info when before a post is destroyed
-  before_destroy do |p|
-    decrement_posts_stats
-  end
+  before_destroy :decrement_posts_stats
+  after_destroy  :rebuild_recent_post
   
 private
   # quickly increment all post stats
   def increment_posts_stats
     # the first post don't count towards counters, so we skip it
     if self.topic.posts.length > 1
-      self.topic.forum.update_attributes(
-        :post_count   => self.topic.forum.post_count + 1,
-        :last_post_id => self.topic.forum.recent_post.nil? ? 0 : self.topic.forum.recent_post.id
-      )
-
+      self.topic.forum.update_attribute(:post_count, self.topic.forum.post_count + 1)
       self.topic.update_attribute(:replies, self.topic.replies + 1)
       self.user.update_attribute(:post_count, self.user.post_count + 1)
     end
@@ -64,14 +61,17 @@ private
   # quickly decrement all post stats
   def decrement_posts_stats
     # first post don't count towards counters, but allow merged post (visible = 3)
-    if (self.topic.posts.length > 1) or (self.visible == 3)
-      self.topic.forum.update_attributes(
-        :post_count   => self.topic.forum.post_count - 1,
-        :last_post_id => self.topic.forum.recent_post.nil? ? 0 : self.topic.forum.recent_post.id
-      )
-
+    if self.topic.posts.length > 1
+      self.topic.forum.update_attribute(:post_count, self.topic.forum.post_count - 1)
       self.topic.update_attribute(:replies, self.topic.replies - 1)
       self.user.update_attribute(:post_count, self.user.post_count - 1)
     end
+  end
+
+  # rebuild the forum's recent post info
+  def rebuild_recent_post
+    self.topic.forum.update_attribute(
+      :last_post_id, self.topic.forum.recent_post.nil? ? 0 : self.topic.forum.recent_post.id
+    )
   end
 end
